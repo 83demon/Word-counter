8a7c3dfc05fb693b15434a853e30aab5b87ea180
vo1 <vkuzko007@gmail.com>
2025-12-25 23:28:04 +0200

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::io;
+
+fn count_characters(string_holder: & mut str) -> usize {
+	string_holder.len()
+}
+
+fn count_words(string_holder: & mut str) -> usize {
+	string_holder.split_whitespace().collect::<Vec<_>>().len()
+}
+
+fn count_lines(string_holder: & mut str) -> usize {
+	string_holder.split('\n').collect::<Vec<_>>().len()
+}
+
+
+fn main() {
+    println!("Hello, world!");
+
+	let mut string_holder = String::new();
+
+	io::stdin()
+		.read_line(& mut string_holder)
+		.expect("A string is expected here");
+
+	let chars_len = count_characters(& mut string_holder);
+	let words_count = count_words(& mut string_holder);
+	let lines_count = count_lines(& mut string_holder);
+
+	println!("You entered {string_holder} with a len of {chars_len} characters and words counter of {words_count}. Number of lines are {lines_count}.");
+}
