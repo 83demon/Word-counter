6b0def583396634aba0ae7a629f77c2e3642914e
vo1 <69196155+83demon@users.noreply.github.com>
2025-12-26 19:41:50 +0200

    Added tests.

--- src/main.rs
+++ src/main.rs
@@ -28,3 +28,34 @@ fn main() {
 
 	println!("You entered {string_holder} with a len of {chars_len} characters and words counter of {words_count}. Number of lines are {lines_count}.");
 }
+
+#[cfg(test)]
+mod test {
+
+	use super::*;
+
+	#[test]
+  	fn test_char_multi_line_count() {
+		let source = "ac s e e\x0Bc f\nr t\tr i\x0As s\rw";
+       	let expected_chars_counter = 26;
+ 		let actual_chars_counter = count_characters(& source);
+        assert_eq!(expected_chars_counter, actual_chars_counter);
+	}
+
+	// \x0B stands for \v, \x0A stands for \f
+	#[test]
+  	fn test_words_multi_line_count() {
+		let source = "ac s e e\x0Bc f\nr t\tr i\x0As s\rw";
+       	let expected_words_counter = 13;
+ 		let actual_words_counter = count_words(& source);
+        assert_eq!(expected_words_counter, actual_words_counter);
+	}
+
+	#[test]
+	fn test_lines_count() {
+		let source = "ac s e e\x0Bc f\nr t\tr i\x0As s\rw";
+		let expected_lines_count = 3;
+		let actual_lines_count = count_lines(& source);
+		assert_eq!(expected_lines_count, actual_lines_count);
+	}
+}
