f5308e1b77408d8027320d68fd4a193ad3c03e25
vo1 <vkuzko007@gmail.com>
2025-12-26 19:02:27 +0200

    Changed parameter type to immutable reference.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::io;
 
-fn count_characters(string_holder: & mut str) -> usize {
+fn count_characters(string_holder: & str) -> usize {
 	string_holder.len()
 }
 
-fn count_words(string_holder: & mut str) -> usize {
+fn count_words(string_holder: & str) -> usize {
 	string_holder.split_whitespace().collect::<Vec<_>>().len()
 }
 
-fn count_lines(string_holder: & mut str) -> usize {
+fn count_lines(string_holder: & str) -> usize {
 	string_holder.split('\n').collect::<Vec<_>>().len()
 }
 
@@ -22,9 +22,9 @@ fn main() {
 		.read_line(& mut string_holder)
 		.expect("A string is expected here");
 
-	let chars_len = count_characters(& mut string_holder);
-	let words_count = count_words(& mut string_holder);
-	let lines_count = count_lines(& mut string_holder);
+	let chars_len = count_characters(& string_holder);
+	let words_count = count_words(& string_holder);
+	let lines_count = count_lines(& string_holder);
 
 	println!("You entered {string_holder} with a len of {chars_len} characters and words counter of {words_count}. Number of lines are {lines_count}.");
 }
